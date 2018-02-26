929aea5871144c5dad00c810bb51a0f5cdc26000
david keller <dakeller@csumb.edu>
2018-02-26 16:40:59 +0000

    json

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> directory </title>
+    </head>
+    <body>
+        <a href="/assinments/hw%202/index.php" > Homework 2 </a>
+    </body>
+</html>
